39f00ee51558d5a9be09fd3c6244457f96fa7df1
rsbadin <newmailforotherthings@gmail.com>
2024-08-27 11:41:15 +0300

    Completable Future media saving methods added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,21 +12,31 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static final int MAX_DEPTH = 3;
+    private static int MAX_DEPTH = 0;
+
     private static final ExecutorService executorService = Executors.newFixedThreadPool(8);
 
     public static void main(String[] args) {
-        String startUrl = "https://example.com";
+//        String startUrl = "https://example.com";
+        Scanner sc = new Scanner(System.in);
+
+//        MAX_DEPTH = Integer.parseInt(sc.nextLine());
+        String startUrl = "https://linuxmasterclub.ru/nomacs/";
         crawl(startUrl, 0, new HashSet<>(), Paths.get("crawled_data"));
         executorService.shutdown();
         try {
@@ -61,36 +71,72 @@ public class Main {
                 crawl(link, depth + 1, visitedUrls, outputDir);
             }
         } catch (IOException e) {
-            System.err.println("Error crawling " + url + ": " + e.getMessage());
+//            System.err.println("Error crawling " + url + ": " + e.getMessage());
         }
     }
 
-    private static void saveMediaContent(Document doc, java.nio.file.Path outputDir) {
-        Elements imgElements = doc.select("img");
-        saveMediaFiles(imgElements, "img", outputDir);
 
+
+    private static List<Future<?>> saveMediaContent(Document doc, Path outputDir) {
+        List<Future<?>> futures = Lists.newArrayList();
+
+        Elements imgElements = doc.select("img");
+        futures.addAll(saveMediaFiles(imgElements, "img", outputDir));
 
         Elements videoElements = doc.select("video");
-        saveMediaFiles(videoElements, "video", outputDir);
+        futures.addAll(saveMediaFiles(videoElements, "video", outputDir));
+
+        return futures;
     }
 
-    private static void saveMediaFiles(Elements elements, String type, java.nio.file.Path outputDir) {
+    private static List<Future<?>> saveMediaFiles(Elements elements, String type, Path outputDir) {
+        List<Future<?>> futures = Lists.newArrayList();
         for (Element element : elements) {
             String url = element.attr("src");
             String fileName = getFileName(url, "." + type);
-            File file = new File(outputDir.toFile(), fileName);
-            if (!file.exists()) {
-                executorService.submit(() -> {
+            Path filePath = Paths.get(outputDir.toString(), fileName);
+            if (!Files.exists(filePath)) {
+                Future<?> future = executorService.submit(() -> {
                     try (InputStream in = new URL(url).openStream()) {
-                        FileUtils.copyInputStreamToFile(in, file);
-                        System.out.println("Downloaded " + type + ": " + file.getAbsolutePath());
+                        Files.copy(in, filePath, StandardCopyOption.REPLACE_EXISTING);
+                        System.out.println("Downloaded " + type + ": " + filePath);
                     } catch (IOException e) {
-                        System.err.println("Error downloading " + type + ": " + url);
-                    }
-                });
-            }
+
+// экспешон
+//                    System.err.println("Error downloading " + type + ": " + url);
+                }
+            });
+            futures.add(future);
         }
     }
+        return futures;
+}
+
+//    private static void saveMediaContent(Document doc, java.nio.file.Path outputDir) {
+//        Elements imgElements = doc.select("img");
+//        saveMediaFiles(imgElements, "img", outputDir);
+//
+//
+//        Elements videoElements = doc.select("video");
+//        saveMediaFiles(videoElements, "video", outputDir);
+//    }
+//    private static void saveMediaFiles(Elements elements, String type, java.nio.file.Path outputDir) {
+//        for (Element element : elements) {
+//            String url = element.attr("src");
+//            String fileName = getFileName(url, "." + type);
+//            File file = new File(outputDir.toFile(), fileName);
+//            if (!file.exists()) {
+//                executorService.submit(() -> {
+//                    try (InputStream in = new URL(url).openStream()) {
+//                        FileUtils.copyInputStreamToFile(in, file);
+//                        System.out.println("Downloaded " + type + ": " + file.getAbsolutePath());
+//                    } catch (IOException e) {
+//                        System.err.println("Error downloading " + type + ": " + url);
+//                    }
+//                });
+//            }
+//        }
+//    }
 
     private static List<String> getLinks(Document doc) {
         Elements linkElements = doc.select("a[href]");
